657a01c24b8c553198900e11d3768849725181c7
ljleifer <wahliytxe@hotmail.com>
2024-01-05 09:52:28 -0600

    Initial Commit

---
+++ Life Utils/Life_UtilsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Life_UtilsApp.swift
+//  Life Utils
+//
+//  Created by Logan Leifer on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Life_UtilsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
